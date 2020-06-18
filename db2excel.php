d042466467080a42bc2e13b0cc83626e40320528
Felipe Morano <fmorano@meat.cl>
2020-06-18 12:56:22 -0400

    chore: basic config

---
+++ db2excel.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: wp-db2excel
+Plugin URI: ''
+Description: Plugins for downloading database tables in excel format
+Version: 2.0
+Author: Felipe Morano
+Author URI: felipemorano.com
+License: GLP2
+*/
