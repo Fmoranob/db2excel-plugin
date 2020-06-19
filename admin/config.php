54029d23b9172487022df19e9eacd228e8f3cd7c
Felipe Morano <fmorano@meat.cl>
2020-06-19 18:10:45 -0400

    feat: add config base tailwing template

--- admin/config.php
+++ admin/config.php
@@ -4,10 +4,5 @@ defined('ABSPATH') or die( "Bye bye" );
 
 // Check admin permisions
 if (! current_user_can ('manage_options')) wp_die (__ ('No tienes suficientes permisos para acceder a esta página.'));
-?>
-	<div class="wrap">
-		<h2><?php _e( 'Table to Excel', 'table2excel' ) ?></h2>
-		Bienvenido a la configuración de ejemplo
-	</div>
-<?php
-?>
+
+include PLUGIN_URL.'/resources/views/config.php';
