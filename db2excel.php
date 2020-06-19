09adf6c36ce4a0321994fbfac9059c3709f21745
Felipe Morano <fmorano@meat.cl>
2020-06-19 18:13:44 -0400

    chore: remove unused code

--- db2excel.php
+++ db2excel.php
@@ -9,25 +9,6 @@ Author URI: felipemorano.com
 License: GLP2
 */
 
-// Call setup when plugin is active
-function pluginprefix_activate() {
-    // Add option to active setup page.
-    add_option('setup_complete', false, '', 'yes');
-}
-register_activation_hook( __FILE__, 'pluginprefix_activate' );
-
-// Add plugin to admin menu
-add_action('admin_menu', 'addToAdminMenu');
-
-function addToAdminMenu(){
-    add_menu_page( 'Descarga de Formularios', 'Descarga de Formularios', 'manage_options', 'bd2excel', 'initPlugin' );
-}
-
-function initPlugin(){
-    require_once('download-db-admin.php');
-}
-
-
 // Evita que un usuario malintencionado ejecute codigo php desde la barra del navegador
 defined('ABSPATH') or die( "Bye bye" );
 
@@ -38,12 +19,3 @@ define('PLUGIN_NAME','Table to Excel');
 //Archivos externos
 include(PLUGIN_ROUTE.'/includes/options.php');
 
-//Se filtran en el toolbar de TinyMCE nº2 los botones de seleccionar fuente, seleccionar tamano y subrayado.
- function rai_nuevos_botones($botones) 
- {	
-	$botones[] = 'fontselect';
-	$botones[] = 'fontsizeselect';
-	$botones[] = 'underline';
-	return $botones;
-}
-add_filter( 'mce_buttons_2','rai_nuevos_botones');
